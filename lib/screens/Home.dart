5d8a7ac8e491e4ced778970dcc2152735e1acf84
moaj257 <moaj257@gmail.com>
2022-07-05 18:00:40 +0530

    Adding content.json

--- lib/screens/Home.dart
+++ lib/screens/Home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
+  final rawUrl = "";
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
